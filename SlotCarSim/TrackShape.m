4c20e09918dc309f82212e5900ea6573799e2c15
belalmksaid <belalmksaid@mgmail.com>
2016-12-04 14:36:30 -0500

    track

--- SlotCarSim/TrackShape.m
+++ SlotCarSim/TrackShape.m
@@ -12,11 +12,18 @@ x = [x ((17.25 - 3.45):-3.45:0)];
 y = [y (-8.25 * ones(1,5))];
 n = 400;
 [xi, yi, tk] = paramSpline(x, y, n);
+figure
+hold on
+plot(xi, yi, '.', 0, 0, 'or',[ -8.25 0], [0 0]);
+text(-5, 1, '8.25 in');
+title('Modeled Track');
+axis equal
 xi = xi - ((max(xi) + min(xi))/2.0);
 yi = yi - ((max(yi) + min(yi))/2.0);
 max(xi) * 2
 x = -1 * xi ./ max(xi);
 y = -1 * yi ./ max(xi);
+figure
 plot(x, y, '.');
 axis equal
 fileID = fopen('track4.txt','w');

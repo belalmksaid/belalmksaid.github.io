8e84149342decf9985a7ba358e128394c4cefe60
belalmksaid <belalmksaid@gmail.com>
2017-06-17 13:28:02 -0400

    add lives

--- PacmanAI/gamecode/pacmanMOD.js
+++ PacmanAI/gamecode/pacmanMOD.js
@@ -329,7 +329,7 @@ Pacman.User = function (game, map) {
 
     function loseLife() { 
         //FITNESS -= 100;
-        //lives -= 1;
+        lives -= 1;
         //if(lives == 0) FITNESS -= 1000;
         FITNESS -= 10;
     };

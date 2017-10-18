011e0dd2dde47aa02084f1f20961b22a4d905be7
Belal Said <belalmksaid@gmail.com>
2017-10-18 10:26:30 -0400

    add sync game

---
+++ projects/MineSweeper/syncgame.js
@@ -0,0 +1,98 @@
+
+class world {
+    constructor() {
+
+    }
+
+    draw(c) {
+        for(let i = 0; i < this.sweepers.length; i++) {
+            let index = -1;
+            let dis = 100000000;
+           for(let j = 0; j < this.pellets.length; j++) {
+               let td = Disque.lengthSqrd(this.sweepers[i].position, this.pellets[j].position);
+               if(td < Math.pow(this.sweepers[i].radius + this.pellets[j].radius, 2)) {
+                   this.sweepers[i].fitness += this.pellets[j].value;
+                   this.pellets[j].reset();
+               }
+               if(td < dis) {
+                   index = j;
+                   dis = td;
+               }
+           }
+           this.sweepers[i].draw(C, this.pellets[index].position.x, this.pellets[index].position.y, this.debug);
+        }
+        for(let i = 0; i < this.pellets.length; i++) {
+           this.pellets[i].draw(C);
+        }
+    }
+}
+
+class sweeper {
+    constructor(pos, or, spd, rad) {
+        this.position = pos;
+        this.orientation = or;
+        this.speed = spd;
+    }
+
+    reset() {
+        this.position = v(Disque.random(0, sandbox.width), Disque.random(0, sandbox.height));
+        this.fitness = 0;
+    }
+
+    update(x, y) {
+        this.position.x += this.speed * Math.cos(this.orientation);
+        this.position.y += this.speed * Math.sin(this.orientation);
+
+        if(this.position.x < 0) {
+            this.position.x = sandbox.width - 1;
+        }
+        else if(this.position.x > sandbox.width) {
+            this.position.x = 1;
+        }
+
+        if(this.position.y < 0) {
+            this.position.y = sandbox.height - 1;
+        }
+        else if(this.position.y > sandbox.height) {
+            this.position.y = 1;
+        }
+
+    }
+
+    draw(c, x, y, d) {
+        this.update(x, y);
+        c.save();
+	    c.translate(this.position.x, this.position.y);
+	    c.rotate(this.orientation);
+	    c.translate(-this.position.x, -this.position.y);
+        rectangleB(c, this.position.x, this.position.y, this.radius, this.radius);
+        rectangleB(c, this.position.x + 0.5 * this.radius, this.position.y, 1.5 * this.radius, 0.3333 * this.radius);
+        c.restore();
+        if(d) {
+            c.beginPath();
+            c.moveTo(this.position.x, this.position.y);
+            c.lineTo(x, y);
+            c.stroke();
+        }
+    }
+}
+
+class pellet {
+    constructor(pos, rad, col) {
+        this.position = pos;
+        this.radius = rad;
+        this.color = col;
+        this.visible = true;
+        this.value = 1;
+    }
+
+    reset() {
+         this.position = v(Disque.random(0, sandbox.width), Disque.random(0, sandbox.height));
+    }
+
+    draw(c) {
+        if(this.visible) {
+            circleF(c, this.position.x, this.position.y, this.radius, this.color);
+        }
+    }
+}

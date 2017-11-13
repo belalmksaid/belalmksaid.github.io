2ede4dab015f018b8abf709f3f968f3d951b046d
belalmksaid <belalmksaid@gmail.com>
2017-11-13 17:39:17 -0500

    update board

--- projects/whiteboard/whiteboard.js
+++ projects/whiteboard/whiteboard.js
@@ -9,6 +9,7 @@ class whiteboard {
         this.penColor = new color(255, 0, 0);
         this.eraserColor = new color(255, 255, 255);
         this.penRadius = 7;
+        this.eraserRadius = 28;
         this.mousePos = v(0, 0);
         this.ismousedown = false;
     }
@@ -20,9 +21,23 @@ class whiteboard {
             this.context.lineCap = 'round';
             this.context.strokeStyle = this.penColor.tostring();
         }
+        if(this.mode == 'eraser') {
+            this.context.lineWidth = this.eraserRadius * 2.0;
+            this.context.lineJoin = 'round';
+            this.context.lineCap = 'round';
+            this.context.strokeStyle = this.eraserColor.tostring();
+        }
         this.ismousedown = true;
         this.context.beginPath();
         this.context.moveTo(this.mousePos.x, this.mousePos.y);
+    }
+
+    mouseup() {
+        this.ismousedown = false;
+    }
+
+    clear() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 
     update() {
@@ -30,7 +45,9 @@ class whiteboard {
     }
 
     draw() {
-        this.context.moveTo(this.mousePos.x, this.mousePos.y);
-        this.context.stroke();
+        if(this.ismousedown) {
+            this.context.lineTo(this.mousePos.x, this.mousePos.y);
+            this.context.stroke();
+        }
     }
 }

56215bd465c39937f441dd43270b5cbc7a679bd5
belalmksaid <belalmksaid@gmail.com>
2017-11-17 01:02:12 -0500

    add web socket connection

--- projects/whiteboard/whiteboard.js
+++ projects/whiteboard/whiteboard.js
@@ -1,4 +1,8 @@
 
+const moveTo = 0;
+const lineTo = 1;
+const clear = 2;
+
 
 class whiteboard {
     constructor(ctx, can) {
@@ -14,6 +18,40 @@ class whiteboard {
         this.ismousedown = false;
         this.canvasLog = new Array();
         this.lock = false;
+        this.guid = -1;
+    }
+
+    init(data) {
+        let arr = JSON.parse(data);
+        for (let i = 0; i < arr.length; i++) {
+            this.guid = arr[i].id;
+            if(arr[i].t == 4) {
+                this.penRadius = arr[i].rr;
+                this.penColor = new color(arr[i].r, arr[i].g, arr[i].b);
+                this.context.lineWidth = this.penRadius * 2.0;
+                this.context.lineJoin = 'round';
+                this.context.lineCap = 'round';
+                this.context.strokeStyle = this.penColor.tostring();
+            }
+            else if(arr[i].t == 3) {
+                this.context.lineWidth = this.eraserRadius * 2.0;
+                this.context.lineJoin = 'round';
+                this.context.lineCap = 'round';
+                this.context.strokeStyle = this.eraserColor.tostring();
+            }
+            else if (arr[i].t == moveTo) {
+                circleF(this.context, arr[i].x, arr[i].y, this.penRadius, this.penColor);
+                this.context.beginPath();
+                this.context.moveTo(arr[i].x, arr[i].y);
+            }
+            else if (arr[i].t == lineTo) {
+                this.context.lineTo(arr[i].x, arr[i].y);
+                this.context.stroke();
+            }
+            else if (arr[i].t == clear) {
+                this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            }
+        }
     }
 
     add(i) {
@@ -23,23 +61,26 @@ class whiteboard {
     }
 
     mousedown() {
-        if(this.mode == 'pen') {
+        if (this.mode == 'pen') {
             this.context.lineWidth = this.penRadius * 2.0;
             this.context.lineJoin = 'round';
             this.context.lineCap = 'round';
-            this.context.strokeStyle = this.penColor.tostring(); 
+            this.context.strokeStyle = this.penColor.tostring();
             circleF(this.context, this.mousePos.x, this.mousePos.y, this.penRadius, this.penColor);
+            this.add({id: -1, t: 4, r: this.penColor.r, g: this.penColor.g, b: this.penColor.b, rr: this.penRadius });
         }
-        if(this.mode == 'eraser') {
+        if (this.mode == 'eraser') {
             this.context.lineWidth = this.eraserRadius * 2.0;
             this.context.lineJoin = 'round';
             this.context.lineCap = 'round';
             this.context.strokeStyle = this.eraserColor.tostring();
+            this.add({id: -1, t: 3, r: this.eraserColor.r, g: this.eraserColor.g, b: this.eraserColor.b, rr: this.eraserRadius });
         }
         this.ismousedown = true;
         this.context.beginPath();
         this.context.moveTo(this.mousePos.x, this.mousePos.y);
-        this.add({t : 0, x : this.mousePos.x, y : this.mousePos.y});
+        
+        this.add({ id: -1, t: 0, x: this.mousePos.x, y: this.mousePos.y });
     }
 
     mouseup() {
@@ -48,30 +89,30 @@ class whiteboard {
 
     clear() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        this.add({t : 2, x : this.mousePos.x, y : this.mousePos.y});
+        this.add({ id: -1, t: 2, x: this.mousePos.x, y: this.mousePos.y });
     }
 
     update() {
-        while(this.lock) {
+        while (this.lock) {
         }
-        if(this.canvasLog.length <= 0) return;
+        if (this.canvasLog.length <= 0) return;
         let clone = this.canvasLog.slice(0);
         $.ajax({
-            url: 'http://api.belalsaid.com/whiteboard/', 
-            type: 'POST', 
-            contentType: 'application/json', 
+            url: 'http://api.belalsaid.com/whiteboard/',
+            type: 'POST',
+            contentType: 'application/json',
             data: JSON.stringify(clone),
-            success: function(data) {
+            success: function (data) {
             }
         });
         this.canvasLog.length = 0;
     }
 
     draw() {
-        if(this.ismousedown) {
+        if (this.ismousedown) {
             this.context.lineTo(this.mousePos.x, this.mousePos.y);
             this.context.stroke();
-            this.add({t : 1, x : this.mousePos.x, y : this.mousePos.y});
+            this.add({ id: -1, t: 1, x: this.mousePos.x, y: this.mousePos.y });
         }
     }
 }

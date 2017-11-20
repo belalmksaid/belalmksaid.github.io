9f5f962133efa255981dfbb7a21391942812169a
belalmksaid <belalmksaid@gmail.com>
2017-11-19 23:22:39 -0500

    debug5

--- blog.js
+++ blog.js
@@ -1,5 +1,4 @@
 function loadBlogs(cont) {
-    console.log(cont);
     let url = "articles/feed.json";
     $.get(url, function(data) {
         console.log(data);
@@ -9,6 +8,7 @@ function loadBlogs(cont) {
         for(let i = 0; i < articles.length; i++) {
             let ele = '<div class="projectPoster"><h1><a href="[url]" target="_blank">[title]</a></h1><h3>[desc]</h3></div>';
             ele = ele.replace("[url]", articles[i].uri).replace("[title]", articles[i].title).replace("[desc]", articles[i].description);
+            console.log(ele);
             cont.innerHTML += ele;
         }
     });

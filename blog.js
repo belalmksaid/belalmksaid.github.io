ce87fdc48bea5d9490e826fc358cf2c8af949e01
belalmksaid <belalmksaid@gmail.com>
2017-11-19 23:26:26 -0500

    fix article url

--- blog.js
+++ blog.js
@@ -5,7 +5,7 @@ function loadBlogs(cont) {
         console.log(articles);
         cont.innerHTML = "";
         for(let i = 0; i < articles.length; i++) {
-            let ele = '<div class="projectPoster"><h1><a href="[url]" target="_blank">[title]</a></h1><h3>[desc]</h3></div>';
+            let ele = '<div class="projectPoster"><h1><a href="articles/[url]" target="_blank">[title]</a></h1><h3>[desc]</h3></div>';
             ele = ele.replace("[url]", articles[i].uri).replace("[title]", articles[i].title).replace("[desc]", articles[i].description);
             console.log(ele);
             cont.innerHTML += ele;

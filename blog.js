fda05a8b1dc03dd11564869f2c190320ee406fe6
belalmksaid <belalmksaid@gmail.com>
2017-11-20 01:24:06 -0500

    view blog

--- blog.js
+++ blog.js
@@ -1,12 +1,24 @@
 function loadBlogs(cont) {
     let url = "articles/feed.json";
-    $.get(url, function(articles) {
+    $.get(url, function (articles) {
         articles = articles.articles;
         cont.innerHTML = "";
-        for(let i = 0; i < articles.length; i++) {
+        for (let i = 0; i < articles.length; i++) {
             let ele = '<div class="projectPoster"><h1><a href="blog.html?id=[id]" target="_blank">[title]</a></h1><h3>[desc]</h3></div>';
             ele = ele.replace("[id]", i.toString()).replace("[title]", articles[i].title).replace("[desc]", articles[i].description);
             cont.innerHTML += ele;
         }
+    });
+}
+
+function loadBlogsFromId(cont, id) {
+    id = parseInt(id);
+    let url = "articles/feed.json";
+    $.get(url, function (articles) {
+        articles = articles.articles;
+        cont.innerHTML = "";
+        let ele = '<iframe src="articles/[url]"></iframe>';
+        ele = ele.replace("[url]", articles[i].uri);
+        cont.innerHTML += ele;
     });
 }

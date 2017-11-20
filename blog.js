dd2fd55eba2144cd52fb0773d99b9853ed9142c3
belalmksaid <belalmksaid@gmail.com>
2017-11-20 17:51:19 -0500

    fix text size

--- blog.js
+++ blog.js
@@ -4,7 +4,7 @@ function loadBlogs(cont) {
         articles = articles.articles;
         cont.innerHTML = "";
         for (let i = 0; i < articles.length; i++) {
-            let ele = '<div class="projectPoster"><h1><a href="blog.html?id=[id]">[title]</a></h1><h3>[desc]</h3><br/><h4>[date]</h4></div>';
+            let ele = '<div class="projectPoster"><h1><a href="blog.html?id=[id]">[title]</a></h1><h3>[desc]</h3><br/><h5>[date]</h5></div>';
             ele = ele.replace("[id]", i.toString()).replace("[title]", articles[i].title).replace("[desc]", articles[i].description).replace("[date]", articles[i].date);
             cont.innerHTML += ele;
         }
